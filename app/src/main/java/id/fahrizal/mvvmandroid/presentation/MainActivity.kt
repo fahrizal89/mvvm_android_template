b473c6010d2076ee6fc16df29c0b433e6207e991
fahrizal.sentosa@dana.id <fahrizal.sentosa@dana.id>
2022-01-24 23:40:57 +0700

    rename ui package to presentation

--- app/src/main/java/id/fahrizal/mvvmandroid/presentation/MainActivity.kt
+++ app/src/main/java/id/fahrizal/mvvmandroid/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package id.fahrizal.mvvmandroid
+package id.fahrizal.mvvmandroid.presentation
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import id.fahrizal.mvvmandroid.R
 import id.fahrizal.mvvmandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {

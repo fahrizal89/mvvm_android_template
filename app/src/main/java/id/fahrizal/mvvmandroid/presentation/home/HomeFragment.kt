1c4946996dc1230384e7c6c0971a5f19e3b38d1e
fahrizal.sentosa@dana.id <fahrizal.sentosa@dana.id>
2022-02-01 22:18:23 +0700

    add PrayUiState with couroutine

--- app/src/main/java/id/fahrizal/mvvmandroid/presentation/home/HomeFragment.kt
+++ app/src/main/java/id/fahrizal/mvvmandroid/presentation/home/HomeFragment.kt
@@ -1,10 +1,16 @@
 package id.fahrizal.mvvmandroid.presentation.home
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.viewbinding.ViewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import id.fahrizal.mvvmandroid.databinding.FragmentHomeBinding
 import id.fahrizal.mvvmandroid.presentation.base.BaseFragment
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
@@ -15,5 +21,29 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun init(viewBinding: ViewBinding) {
         homeViewModel.getPraySchedule()
+        fetchPray()
+    }
+
+    private fun fetchPray() {
+        lifecycleScope.launch {
+            // repeatOnLifecycle launches the block in a new coroutine every time the
+            // lifecycle is in the STARTED state (or above) and cancels it when it's STOPPED.
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                homeViewModel.uiState.collect {
+                    when (it) {
+                        is HomeViewModel.PrayUiState.Loaded -> onLoaded()
+                        else -> showLoading()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun onLoaded() {
+        Timber.d("onLoaded")
+    }
+
+    private fun showLoading() {
+        Timber.d("showLoading")
     }
 }
--- app/src/main/java/id/fahrizal/mvvmandroid/presentation/home/HomeViewModel.kt
+++ app/src/main/java/id/fahrizal/mvvmandroid/presentation/home/HomeViewModel.kt
@@ -8,6 +8,8 @@ import id.fahrizal.mvvmandroid.domain.schedule.usecase.GetPraySchedules
 import id.fahrizal.mvvmandroid.util.TimeUtil
 import id.fahrizal.mvvmandroid.util.rx.ObservableExt.observe
 import id.fahrizal.mvvmandroid.util.rx.SimpleSingleObserver
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import java.util.*
 import javax.inject.Inject
 
@@ -16,13 +18,16 @@ class HomeViewModel @Inject constructor(
     private val getPraySchedules: GetPraySchedules
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow<PrayUiState>(PrayUiState.Empty)
+    val uiState: StateFlow<PrayUiState> = _uiState
+
     fun getPraySchedule() {
         val requestParam = PrayScheduleRequest("jakarta", getTodayDate())
 
         getPraySchedules.execute(requestParam)
             .observe(object : SimpleSingleObserver<List<PraySchedule>>() {
                 override fun onSuccessful(response: List<PraySchedule>) {
-                    response.size
+                    _uiState.value = PrayUiState.Loaded(response)
                 }
 
                 override fun onError(message: String) {
@@ -32,4 +37,11 @@ class HomeViewModel @Inject constructor(
     }
 
     private fun getTodayDate() = TimeUtil.getDateFormatted(Date())
+
+    sealed class PrayUiState {
+        object Empty : PrayUiState()
+        object Loading : PrayUiState()
+        class Loaded(val data: List<PraySchedule>) : PrayUiState()
+        class Error(val message: String) : PrayUiState()
+    }
 }
